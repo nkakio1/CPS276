5dc2391ff6255a55d72c7875c2c5023e3c881d4c
nkakio1 <104183246+nkakio1@users.noreply.github.com>
2025-09-29 16:41:33 -0400

    ss

--- Assn3/index.php
+++ Assn3/index.php
@@ -1,10 +1,47 @@
 <?php
-$output = "";
+$output = [];
 
-if($_SERVER["REQUEST_METHOD"] == "POST"){
- require_once 'processNames.php'>
- $output = addClearNames();
+if ($_SERVER["REQUEST_METHOD"] === "POST") {
+    require_once 'processNames.php';
+    $output = addClearNames();
 }
-?>`
+?>
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet"
+          href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css"
+          crossorigin="anonymous">
+    <title>NameListHolder</title>
+  </head>
+  <body>
+    <div class="container">
+      <h1>NameListHolder</h1>
 
-<!DOCTYPE html>
+      <form method="post" action="index.php">
+
+        <input type="submit" name="addName" class="btn btn-primary" value="Add Name">
+        <input type="submit" name="clearNames" class="btn btn-primary" value="Clear Names">
+
+        <div class="form-group mt-3">
+          <label for="name">Enter Name (First Last)</label>
+          <input type="text" class="form-control" id="name" name="name" autocomplete="off">
+        </div>
+
+        <div class="form-group mt-3">
+          <label for="namelist">List of Names</label>
+          <textarea style="height: 500px;" class="form-control" id="namelist"><?php
+           
+            echo displayNames($output);
+          ?></textarea>
+        </div>
+
+        
+        <input type="hidden" name="namelist" value='<?php echo htmlspecialchars(json_encode($output)); ?>'>
+
+      </form>
+    </div>
+  </body>
+</html>
--- Assn3/processNames.php
+++ Assn3/processNames.php
@@ -1,29 +1,54 @@
-<!doctype html>
-<html lang="en">
-  <head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+<?php
+function addClearNames() {
+    $names = [];
 
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    if (isset($_POST['namelist']) && !empty($_POST['namelist'])) {
+        $names = json_decode($_POST['namelist'], true) ?? [];
+    }
 
-    <title>add names</title>
-  </head>
-  <body>
-    <div class="container">
-    <h1>Add Names</h1>
-    <form method="post" action="index.php">
-    <input type="submit" name="addName" class="btn btn-primary" value="Add Name">
-    <input type="submit" name="clearNames" class="btn btn-primary" value="Clear Names">
-    <div class="form-group">
-      <label for="name">Enter Name</label>
-      <input type="text" class="form-control" id="name" name="name" >
-    </div>
-    <div class="form-group">
-      <label for="namelist">List of Names</label>
-      <textarea style="height: 500px;" class="form-control" id="namelist" name="namelist"></textarea>
-    </div>
-    
-  </form>
-</div>
+    if (isset($_POST['addName']) && !empty(trim($_POST['name']))) {
+        $fullName = trim($_POST['name']);
+        $parts = explode(' ', $fullName, 2);
+        $firstName = ucfirst(strtolower($parts[0] ?? ''));
+        $lastName  = ucfirst(strtolower($parts[1] ?? ''));
+
+        array_unshift($names, [
+            'first' => $firstName,
+            'last'  => $lastName
+        ]);
+    }
+
+    if (isset($_POST['clearNames'])) {
+        $names = [];
+    }
+
+    return $names;
+}
+
+function displayNames($names) {
+    $output = "";
+    if (!empty($names)) {
+        foreach ($names as $person) {
+            $output .= htmlspecialchars(trim($person['first'] . ' ' . $person['last'])) . "\n";
+        }
+    }
+    return $output;
+}
+/*
+//What is the purpose of separating the functionality between index.php and processNames.php?
+//
+//
+//How does the $_SERVER["REQUEST_METHOD"] variable help determine when to process form submissions in PHP?
+//
+//
+//How does PHP handle string-to-array conversion using the explode function, and why is this useful in this application?
+//
+//What role does the implode function play in formatting the output for the textarea?
+
+
+//How does the use of "\n" inside a double-quoted string affect how names are displayed in the textarea? Why not use <br>?
+
+//What security considerations should be taken into account when handling user input in this application?
+*/
+?>
+
